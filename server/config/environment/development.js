4342c5537ac82ffc43bd82da1f3fa010f7de1f6c
MeanDev16 <kondalrao_mca@yahoo.co.in>
2016-11-03 17:17:18 +0530

    follow9

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://elecart-6653:1d_gH_PX6SS6PqvVwwaA@elecart-6653.mongo.dbs.appsdeck.eu:30444/elecart-6653' || process.env.MONGOLAB_URI || 'mongodb://localhost/shopnx-dev'
+    uri: 'mongodb://elecart-6653:1d_gH_PX6SS6PqvVwwaA@elecart-6653.mongo.dbs.appsdeck.eu:30444/elecart-6653' || 'mongodb://<dbuser>:<dbpassword>@ds153845.mlab.com:53845/heroku_wn6prhj7' || 'mongodb://localhost/shopnx-dev'
   },
 
   seedDB: true
