ad2d8e15d4e343145693b24be973b1583806db8b
MeanDev16 <kondalrao_mca@yahoo.co.in>
2016-11-03 17:29:40 +0530

    follow7

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,8 +5,12 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://elecart-6653:1d_gH_PX6SS6PqvVwwaA@elecart-6653.mongo.dbs.appsdeck.eu:30444/elecart-6653' || 'mongodb://<dbuser>:<dbpassword>@ds153845.mlab.com:53845/heroku_wn6prhj7' || 'mongodb://localhost/shopnx-dev'
+    uri: 'mongodb://<user1>:<user1>@ds153845.mlab.com:53845/heroku_wn6prhj7' || 'mongodb://localhost/shopnx-dev'
   },
 
   seedDB: true
 };
+
+
+
+// scalingo : 'mongodb://elecart-6653:1d_gH_PX6SS6PqvVwwaA@elecart-6653.mongo.dbs.appsdeck.eu:30444/elecart-6653' 
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -13,7 +13,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:   'mongodb://elecart-6653:1d_gH_PX6SS6PqvVwwaA@elecart-6653.mongo.dbs.appsdeck.eu:30444/elecart-6653' || 'mongodb://<dbuser>:<dbpassword>@ds153845.mlab.com:53845/heroku_wn6prhj7' || 'mongodb://localhost/shopnx'
+    uri:   'mongodb://<user1>:<user1>@ds153845.mlab.com:53845/heroku_wn6prhj7' || 'mongodb://localhost/shopnx'
   },
 
   seedDB: true
